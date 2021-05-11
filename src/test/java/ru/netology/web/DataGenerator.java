0f9a7bebb58be479673b58faff96eea5d8479e3d
Ekaterina <46753604+troffimovka@users.noreply.github.com>
2021-05-11 21:52:19 +0300

    Update DataGenerator.java

--- src/test/java/ru/netology/web/DataGenerator.java
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -8,84 +8,48 @@ import io.restassured.specification.RequestSpecification;
 import lombok.Data;
 
 import java.util.Locale;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import static io.restassured.RestAssured.given;
 
-@Data
 public class DataGenerator {
+    private static Faker faker = new Faker(new Locale("en"));
 
-    public static class Autharization {
-        private Autharization() {
-        }
-
-        public static ClientData generateAutharizationForActiveUser() {
-            Faker faker = new Faker(new Locale("en"));
-            return new ClientData(
-                    faker.name().username(),
-                    faker.internet().password(),
-                    "active");
-        }
-
-        public static ClientData generateAutharizationForBlockedUser() {
-            Faker faker = new Faker(new Locale("en"));
-            return new ClientData(
-                    faker.name().username(),
-                    faker.internet().password(),
-                    "blocked");
-        }
-
-        private static RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("http://localhost")
-                .setPort(9999)
-                .setAccept(ContentType.JSON)
-                .setContentType(ContentType.JSON)
-                .log(LogDetail.ALL)
-                .build();
-
-        public static ClientData registrationOfActiveUser() {
-            ClientData clientData = generateAutharizationForActiveUser();
-            Gson gsonBuilder = new GsonBuilder().create();
-            String jsonClientData = gsonBuilder.toJson(clientData);
-            given()
-                    .spec(requestSpec)
-                    .body(jsonClientData)
-                    .when()
-                    .post("/api/system/users")
-                    .then()
-                    .statusCode(200);
-            return clientData;
-        }
-
-        public static ClientData registrationOfBlockedUser() {
-            ClientData clientData = generateAutharizationForBlockedUser();
-            Gson gsonBuilder = new GsonBuilder().create();
-            String jsonClientData = gsonBuilder.toJson(clientData);
-            given()
-                    .spec(requestSpec)
-                    .body(jsonClientData)
-                    .when()
-                    .post("/api/system/users")
-                    .then()
-                    .statusCode(200);
-            return clientData;
-        }
-
-        public static String incorrectLogin() {
-            Faker faker = new Faker(new Locale("en"));
-            return faker.name().username();
-        }
-
-        public static String incorrectPassword() {
-            Faker faker = new Faker(new Locale("en"));
-            return faker.internet().password();
-        }
+    private DataGenerator() {
     }
+
+    public static RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+
+    public static void setNewUser(UserInfo registration) {
+        given()
+                .spec(requestSpecification) // указываем, какую спецификацию используем
+                .body(registration) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200);
+    }
+
+
+    public static UserInfo getRegisteredUser(String status) {
+        String login = faker.name().fullName();
+        String password = faker.internet().password();
+        UserInfo registration = new UserInfo(login, password, status);
+        setNewUser(registration);
+        return registration;
+    }
+
+    public static String getNewLogin(){
+        return faker.name().fullName();
+    }
+
+    public static String getNewPassword(){
+        return faker.internet().password();
+    }
+
 }
-
-
-
-
-
